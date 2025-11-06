2b4d3d8d5e9404f21f1d88f65d5b47686ae21f20
Roy Vakil <YOUR_EMAIL@users.noreply.github.com>
2025-11-06 22:33:15 +0200

    Enable Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import Background from './components/Background'
 import Header from './components/Header'
 import Hero from './components/Hero'
@@ -19,6 +20,7 @@ function App() {
         <ContactForm />
       </main>
       <Footer />
+      <Analytics />
     </div>
   )
 }
